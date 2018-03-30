cb0a5a9493332e8928e8170a1116bb951419b991
Frank Li <li.26.frank@gmail.com>
2018-03-29 22:02:36 -0400

    Burger tells user to add ingredients if there are none

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  // transform props into array of BurgerIngredient components
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) =>
         <BurgerIngredient key={igKey + i} type={igKey} />
       );
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0)
+    transformedIngredients = <p>Please start adding ingredients</p>;
 
   return (
     <div className={classes.Burger}>
