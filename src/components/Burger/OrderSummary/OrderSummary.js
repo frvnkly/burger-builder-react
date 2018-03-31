287e5c5daf9dc081bcec9c4963d42e70814fb7b6
Frank Li <li.26.frank@gmail.com>
2018-03-31 01:16:00 -0400

    Modal component with css displaying OrderSummary

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const orderSummary = props => {
+	const ingredientsSummary = Object.keys(props.ingredients)
+		.map(igKey => 
+			<li key={igKey}>
+				<span style={{textTransform: 'capitalize'}}>{igKey}</span>
+				: {props.ingredients[igKey]}
+			</li>
+		);
+
+	return (
+		<React.Fragment>
+			<h3>Your Order</h3>
+			<p>A delicious burger with the following ingredients:</p>
+			<ul>
+				{ingredientsSummary}
+			</ul>
+			<p>Continue to checkout?</p>
+		</React.Fragment>
+	);
+};
+
+export default orderSummary;
