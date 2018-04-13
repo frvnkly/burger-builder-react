2b2060fba632c87f7f8ff861942c8902a2ee90ca
Frank Li <li.26.frank@gmail.com>
2018-04-12 22:46:03 -0400

    OrderSummary modal directs to Checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,24 +83,25 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!');
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'John Smith',
-        address: {
-          street: '123 Test Street',
-          zipCode: '12345',
-          country: 'United States',
-        },
-        email: 'test@test.com',
-      },
-      deliveryMethod: 'fastest',
-    };
-    axios.post('/orders.json', order)
-      .then(response => this.setState({ loading: false, purchasing: false }))
-      .catch(error => this.setState({ loading: false, purchasing: false }));
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'John Smith',
+    //     address: {
+    //       street: '123 Test Street',
+    //       zipCode: '12345',
+    //       country: 'United States',
+    //     },
+    //     email: 'test@test.com',
+    //   },
+    //   deliveryMethod: 'fastest',
+    // };
+    // axios.post('/orders.json', order)
+    //   .then(response => this.setState({ loading: false, purchasing: false }))
+    //   .catch(error => this.setState({ loading: false, purchasing: false }));
+    this.props.history.push('/checkout');
   };
 
   render() {
