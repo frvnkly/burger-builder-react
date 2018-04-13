077856eb4ed101dd18b426f29f81379c0e0b0f99
Frank Li <li.26.frank@gmail.com>
2018-04-13 00:29:05 -0400

    Checkout sends order info to backend

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,30 +83,13 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!');
-    // this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'John Smith',
-    //     address: {
-    //       street: '123 Test Street',
-    //       zipCode: '12345',
-    //       country: 'United States',
-    //     },
-    //     email: 'test@test.com',
-    //   },
-    //   deliveryMethod: 'fastest',
-    // };
-    // axios.post('/orders.json', order)
-    //   .then(response => this.setState({ loading: false, purchasing: false }))
-    //   .catch(error => this.setState({ loading: false, purchasing: false }));
     const queryParams = [];
     for (const i in this.state.ingredients) {
       queryParams.push(encodeURIComponent(i) 
                        + '=' 
                        + encodeURIComponent(this.state.ingredients[i]));
     }
+    queryParams.push(`price=${this.state.totalPrice}`);
     const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: '/checkout',
