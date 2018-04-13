34585e498cc52bf7039ae97b30192ff595b50e98
Frank Li <li.26.frank@gmail.com>
2018-04-13 00:56:49 -0400

    navbar directs to Orders page and indicates active route

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,10 +3,12 @@ import React from 'react';
 import classes from './Order.css';
 
 const order = props => {
-  <div className={classes.Order}>
-    <p>Ingredients: Salad (1)</p>
-    <p>Price: <strong>$5.45</strong></p>
-  </div>
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: Salad (1)</p>
+      <p>Price: <strong>$5.45</strong></p>
+    </div>
+  );
 }
 
 export default order;
