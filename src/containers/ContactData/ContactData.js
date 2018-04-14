f0577f040271c41fa542d11fc07347b04965f615
Frank Li <li.26.frank@gmail.com>
2018-04-14 00:01:28 -0400

    contact form validation

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -16,6 +16,11 @@ class ContactData extends Component {
           placeholder: 'Your Name',
         },
         value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
       },
       street: {
         elementType: 'input',
@@ -24,6 +29,11 @@ class ContactData extends Component {
           placeholder: 'Street',
         },
         value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
       },
       zipCode: {
         elementType: 'input',
@@ -32,6 +42,13 @@ class ContactData extends Component {
           placeholder: 'ZIP Code',
         },
         value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5,
+        },
+        valid: false,
+        touched: false,
       },
       country: {
         elementType: 'input',
@@ -40,6 +57,11 @@ class ContactData extends Component {
           placeholder: 'Country',
         },
         value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
       },
       email: {
         elementType: 'input',
@@ -48,6 +70,11 @@ class ContactData extends Component {
           placeholder: 'Your E-Mail',
         },
         value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
       },
       deliveryMethod: {
         elementType: 'select',
@@ -61,10 +88,25 @@ class ContactData extends Component {
             displayValue: 'Cheapest',
           }]
         },
-        value: '',
+        value: 'fastest',
+        validation: {},
+        valid: true,
+        touched: false,
       },
     },
+    formIsValid: false,
     loading: false,
+  }
+
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules.required) 
+      isValid = value.trim() !== '' && isValid;
+    if (rules.minLength)
+      isValid = value.length >= rules.minLength && isValid;
+    if (rules.maxLength)
+      isValid = value.length <= rules.maxLength && isValid;
+    return isValid;
   }
 
   orderHandler = event => {
@@ -91,8 +133,19 @@ class ContactData extends Component {
     const updatedOrderForm = { ...this.state.orderForm };
     const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+    updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    this.setState({ orderForm: updatedOrderForm });
+
+    let formIsValid = true;
+    for (const inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+
+    this.setState({
+      orderForm: updatedOrderForm,
+      formIsValid: formIsValid,
+    });
   }
 
   render() {
@@ -110,10 +163,18 @@ class ContactData extends Component {
             key={formElement.id}
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             value={formElement.config.value}
             changed={event => this.inputChangedHandler(event, formElement.id)} />
           ))}
-        <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
+        <Button 
+          btnType='Success' 
+          clicked={this.orderHandler}
+          disabled={!this.state.formIsValid}>
+            ORDER
+        </Button>
       </form>;
     if (this.state.loading) form = <Spinner />;
     
