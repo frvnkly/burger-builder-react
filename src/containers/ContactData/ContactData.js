2569671b37f8ad3b4d80d811846e83b652030ddb
Frank Li <li.26.frank@gmail.com>
2018-04-23 02:00:26 -0400

    ContactData form and Orders history guarded by authentication

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -122,7 +122,7 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData,
     };
-    this.props.onOrderBurger(order);
+    this.props.onOrderBurger(order, this.props.token);
   }
 
   inputChangedHandler = (event, inputIdentifier) => {
@@ -188,13 +188,14 @@ const mapStateToProps = state => {
     ingredients: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
+    token: state.auth.token,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: orderData => 
-      dispatch(actions.purchaseBurger(orderData)),
+    onOrderBurger: (orderData, token) => 
+      dispatch(actions.purchaseBurger(orderData, token)),
   };
 };
 
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -10,7 +10,7 @@ import * as actions from '../../store/actions';
 class Orders extends Component {
 
   componentDidMount() {
-    this.props.onFetchOrders();
+    this.props.onFetchOrders(this.props.token);
   }
 
   render() {
@@ -34,12 +34,13 @@ const mapStateToProps = state => {
   return {
     orders: state.order.orders,
     loading: state.order.loading,
+    token: state.auth.token,
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onFetchOrders: () => dispatch(actions.fetchOrders()),
+    onFetchOrders: token => dispatch(actions.fetchOrders(token)),
   };
 };
 
