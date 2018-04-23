8d6618661331481978fa8e06924f66914a5ddb10
Frank Li <li.26.frank@gmail.com>
2018-04-22 21:42:49 -0400

    app logs user out when token expires

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -25,6 +25,20 @@ export const authFail = error => {
   };
 };
 
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
+
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+  };
+};
+
 export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
@@ -41,6 +55,7 @@ export const auth = (email, password, isSignup) => {
       .then(res => {
         console.log(res);
         dispatch(authSuccess(res.data.idToken, res.data.localId));
+        dispatch(checkAuthTimeout(res.data.expiresIn));
       })
       .catch(err => {
         dispatch(authFail(err.response.data.error));
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -13,6 +13,7 @@ const reducer = (state=initialState, action) => {
     case (actionTypes.AUTH_START): return authStart(state, action);
     case (actionTypes.AUTH_SUCCESS): return authSuccess(state, action);
     case (actionTypes.AUTH_FAIL): return authFail(state, action);
+    case (actionTypes.AUTH_LOGOUT): return authLogout(state, action);
     default: return state;
   }
 };
@@ -34,6 +35,10 @@ const authFail = (state, action) => {
     error: action.error,
     loading: false,
   });
+};
+
+const authLogout = (state, action) => {
+  return updateObject(state, { token: null, userId: null });
 };
 
 export default reducer;
