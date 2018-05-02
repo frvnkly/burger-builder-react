2ce65bd8faafeb77aec8a58b57e58a821af7c9a5
Frank Li <li.26.frank@gmail.com>
2018-05-01 22:06:55 -0400

    app only displays orders made by user

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -121,6 +121,7 @@ class ContactData extends Component {
       ingredients: this.props.ingredients,
       price: this.props.price,
       orderData: formData,
+      userId: this.props.userId,
     };
     this.props.onOrderBurger(order, this.props.token);
   }
@@ -189,6 +190,7 @@ const mapStateToProps = state => {
     price: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
     token: state.auth.token,
+    userId: state.auth.userId,
   };
 };
 
